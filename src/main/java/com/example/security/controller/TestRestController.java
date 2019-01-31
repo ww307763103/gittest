5322691b1e1a41f21f97dbbc72315b5b7944b914
ww <307763103@qq.com>
2019-01-31 10:21:14 +0800

    测试冲突

--- src/main/java/com/example/security/controller/TestRestController.java
+++ src/main/java/com/example/security/controller/TestRestController.java
@@ -1,7 +1,12 @@
 package com.example.security.controller;
 
+import com.example.security.config.RedisConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author DELL
@@ -9,9 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestRestController {
+
+    /**
+     * request对象
+     */
+    @Autowired
+    private HttpServletRequest request;
+
+    /*@Autowired
+    private RedisConfig redisConfig;*/
+    @Value("${spring.redis.port: #{6666}}")
+    private Integer port;
+
+    /**
+     * 测试
+     * @return 返回
+     */
     @RequestMapping("/hello")
-    public String helloWorld() {
-        return "spring security hello world";
+    public Integer helloWorld() {
+       return port;
     }
 
 }
